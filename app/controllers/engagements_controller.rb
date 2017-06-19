9de2d91afa3cf90dc3451bacb3b704424aea5d13
tonychinmy <tonychin0101@gmail.com>
2017-06-19 19:04:28 +0800

    requests offers engagements and reviews routes and stuff

--- app/controllers/engagements_controller.rb
+++ app/controllers/engagements_controller.rb
@@ -8,14 +8,27 @@ class EngagementsController < ApplicationController
 		@engagement = Engagement.new
 	end
 
-	def create
-		e = Engagement.new
-			e.user_id = current_user.id
-			e.offer_id = params[:offer_id]
-		e.save
+	def create 
+		@offer = Offer.find(params[:offer_id])
+		@request = Request.find(params[:request_id])
 
+		e = Engagement.new
+		e.user_id = current_user.id
+		
+		if @offer		
+			e.offer_id = params[:offer_id]
+			e.save
 			flash[:notice] = "Successful"
-			redirect_to root_path
+			# redirect_to root_path
+			redirect_to offer_engagement_path(@offer, e)
+		elsif @request
+			e.request_id = params[:request_id]
+			e.save
+			flash[:notice] = "Successful"
+			# redirect_to root_path
+			redirect_to request_engagement_path(@request, e)
+		end
+
 	end
 
 	def show
